b11ea23bed16a75d8779d85e87c794e8d1c66539
noy2high <zyrobuddy@gmail.com>
2024-08-24 03:10:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const loader = document.querySelector('.loader');
-    const content = document.querySelector('.content');
-
-    if (loader && content) {
-        setTimeout(() => {
-            loader.classList.add('hide-loader'); // Hide the loader
-            content.style.display = 'block';     // Show the content
-        }, 7000); // Match the duration of the delay (7s in this case)
-    } else {
-        console.error('Loader or content not found');
-    }
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.querySelector('.loader').classList.add('hide-loader');
+        document.querySelector('.content').classList.remove('hide-loader');
+    }, 7000); // 7 seconds delay
 });
