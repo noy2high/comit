d132ea5272bed12af0c14f3ae3cd639dd27e38f8
noy2high <zyrobuddy@gmail.com>
2024-08-24 03:32:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Play audio after a delay to sync with the loading animation (removed if not used)
     setTimeout(function() {
         document.querySelector('.loader').classList.add('hide-loader');
         document.querySelector('.content').classList.remove('hide-loader');
+        document.querySelector('.content').classList.add('show-content');
     }, 7000); // 7 seconds delay
 });
