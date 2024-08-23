5c04d561d2d9a243ebed95ca1753d88cc898cc4a
noy2high <zyrobuddy@gmail.com>
2024-08-24 02:49:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get loader and content elements
     const loader = document.querySelector('.loader');
     const content = document.querySelector('.content');
 
-    // Check if loader and content exist
     if (loader && content) {
-        // Show the content only after the animation has completed
         setTimeout(() => {
             loader.classList.add('hide-loader'); // Hide the loader
             content.style.display = 'block';     // Show the content
