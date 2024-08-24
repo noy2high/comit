b6dd3dda47e39c943c84266949cd0baf6912e863
noy2high <zyrobuddy@gmail.com>
2024-08-25 01:43:50 +0700

    Update script.js
    refined bg 3

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Hide the loader and show content after 7 seconds
-    setTimeout(function() {
+window.addEventListener('load', () => {
+    setTimeout(() => {
         document.getElementById('loader').style.display = 'none';
-        document.getElementById('content').style.display = 'flex'; // Ensure content is displayed
-    }, 7000);
+        document.getElementById('content').classList.add('show');
+    }, 7000); // 7 seconds
 });
