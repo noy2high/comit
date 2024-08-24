e0a82629fcceb8491dbc5602c0395022ff191314
noy2high <zyrobuddy@gmail.com>
2024-08-25 02:17:30 +0700

    Update script.js
    loading 3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-window.addEventListener('load', () => {
+document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
         document.getElementById('loader').style.display = 'none';
         document.getElementById('content').classList.add('show');
-    }, 7000); // 7 seconds
+    }, 7000); // Show content after 7 seconds
 });
