9288baaf70c85799d341cd05f609eadd1bc0d8b6
noy2high <zyrobuddy@gmail.com>
2024-08-24 13:59:44 +0700

    Update script.js
    new loading

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Simulate a loading process
     setTimeout(function() {
-        document.querySelector('.loader').style.display = 'none'; // Hide the loader
-        document.querySelector('.content').style.display = 'block'; // Show the content
-    }, 3000); // Adjust time as needed (3000ms = 3 seconds)
+        document.querySelector('.loader').style.display = 'none';
+        document.querySelector('.content').style.display = 'block';
+    }, 3000);
 });
