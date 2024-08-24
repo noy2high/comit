a49121931f49e1102e53701cae04f125bab357d8
noy2high <zyrobuddy@gmail.com>
2024-08-24 12:31:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Delay showing the content until the loader completes
+    // Wait for the content to be fully loaded before showing it
     setTimeout(function() {
         document.querySelector('.loader').style.display = 'none'; // Hide the loader
         document.querySelector('.content').style.display = 'block'; // Show the content
