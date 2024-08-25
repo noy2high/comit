d8626821c0b3dbb2dbe1f9424ef5dd1723cb3591
noy2high <zyrobuddy@gmail.com>
2024-08-26 02:07:55 +0700

    Update script.js
    pls works

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-        document.getElementById("loader").style.display = "none";
-        document.getElementById("content").style.display = "flex";
-    }, 7000); // 7 seconds delay
+window.addEventListener('load', function() {
+  // Hide loader and show content after 7 seconds
+  setTimeout(function() {
+    document.getElementById('loader').style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+  }, 7000); // 7 seconds
 });
