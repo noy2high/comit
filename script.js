6b6e4f82afa7237a2256aa552a04c6c150bca8f6
noy2high <zyrobuddy@gmail.com>
2024-08-26 01:36:28 +0700

    Update script.js
    loading 2

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ window.addEventListener("load", function() {
     setTimeout(function() {
         document.getElementById("loader").style.display = "none";
         document.getElementById("content").style.display = "block";
+        document.getElementById("overlay").style.zIndex = "0";
     }, 7000);
 });
