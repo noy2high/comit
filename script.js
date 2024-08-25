f82c690e1345bb674ee01d3d36fb5e3fd31511a4
noy2high <zyrobuddy@gmail.com>
2024-08-26 02:15:19 +0700

    Update script.js
    asd

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-window.addEventListener('load', function() {
-  // Hide loader and show content after 7 seconds
-  setTimeout(function() {
-    document.getElementById('loader').style.display = 'none';
-    document.getElementById('content').style.display = 'block';
-  }, 7000); // 7 seconds
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        document.getElementById('loader').style.display = 'none';
+        document.getElementById('content').classList.remove('hidden');
+    }, 7000); // 7 seconds delay
 });
