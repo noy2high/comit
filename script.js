0b4fd6d92bdfa8b4b2fc2dbf5016905b184a99a4
noy2high <zyrobuddy@gmail.com>
2024-08-26 01:33:27 +0700

    Update script.js
    loading 1

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Get elements
-    const loader = document.getElementById('loader');
-    const content = document.getElementById('content');
-    
-    // Show content after 7 seconds
-    setTimeout(() => {
-        loader.style.display = 'none'; // Hide the loader
-        content.classList.remove('hidden'); // Show the content
+window.addEventListener("load", function() {
+    setTimeout(function() {
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("content").style.display = "block";
     }, 7000);
 });
